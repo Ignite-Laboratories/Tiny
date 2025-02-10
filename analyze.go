242d0c67480f0fc9f66bf196bfb8ad5d61556370
Robert Petz <rpetz@ignitelabs.net>
2025-02-10 13:32:30 -0800

    Finished moving everything to variadic functions instead of slices

--- analyze.go
+++ analyze.go
@@ -6,7 +6,7 @@ import "bytes"
 type analyze struct{}
 
 // BitShade counts the number of 1s and 0s in the data.
-func (_ analyze) BitShade(bits []Bit) BinaryCount {
+func (_ analyze) BitShade(bits ...Bit) BinaryCount {
 	count := BinaryCount{}
 
 	for i := 0; i < len(bits); i++ {
@@ -23,28 +23,28 @@ func (_ analyze) BitShade(bits []Bit) BinaryCount {
 }
 
 // DataShade checks if the number of ones in the data exceeds half it's the length.
-func (a analyze) DataShade(data []byte) BinaryCount {
+func (a analyze) DataShade(data ...byte) BinaryCount {
 	count := BinaryCount{}
 
 	for i := 0; i < len(data); i++ {
-		c := a.BitShade(From.Byte(data[i]))
+		c := a.BitShade(From.Byte(data[i])...)
 		count.Ones += c.Ones
 		count.Zeros += c.Zeros
 		count.Total += c.Total
 	}
-	count.Distribution = a.OneDistribution(data)
+	count.Distribution = a.OneDistribution(data...)
 	count.calculate()
 
 	return count
 }
 
 // HasPrefix upcasts the input slices to bytes and then calls bytes.HasPrefix.
-func (_ analyze) HasPrefix(data []Bit, pattern []Bit) bool {
+func (_ analyze) HasPrefix(data []Bit, pattern ...Bit) bool {
 	return bytes.HasPrefix(subToByte(data), subToByte(pattern))
 }
 
-// OneDistribution counts how many ones occupy each position of the provided slice of bytes.
-func (_ analyze) OneDistribution(data []byte) [8]int {
+// OneDistribution counts how many ones occupy each position of the provided bytes.
+func (_ analyze) OneDistribution(data ...byte) [8]int {
 	output := [8]int{}
 	for _, b := range data {
 		for i := 0; i < 8; i++ {
--- modify.go
+++ modify.go
@@ -5,8 +5,8 @@ import "log"
 // modify is a way to alter existing binary information.
 type modify struct{}
 
-// XORWithPattern XORs a pattern of bits against a byte, starting at the most significant bit.
-func (_ modify) XORWithPattern(b byte, pattern []Bit) byte {
+// XORBitsWithPattern XORs a pattern of bits against a byte, starting at the most significant bit.
+func (_ modify) XORBitsWithPattern(b byte, pattern ...Bit) byte {
 	if len(pattern) > 8 {
 		log.Fatalf("input pattern should not be larger than a byte")
 	}
@@ -19,24 +19,24 @@ func (_ modify) XORWithPattern(b byte, pattern []Bit) byte {
 	return To.Byte(bits...)
 }
 
-// XORDataWithPattern XORs a pattern of bits against every byte, starting at the most significant bit of each.
-func (m modify) XORDataWithPattern(data []byte, pattern []Bit) []byte {
+// XORBytesWithPattern XORs a pattern of bits against every byte, starting at the most significant bit of each.
+func (m modify) XORBytesWithPattern(data []byte, pattern ...Bit) []byte {
 	for i := 0; i < len(data); i++ {
-		data[i] = m.XORWithPattern(data[i], pattern)
+		data[i] = m.XORBitsWithPattern(data[i], pattern...)
 	}
 	return data
 }
 
 // ToggleBits XORs every bit with 1.
-func (_ modify) ToggleBits(bits []Bit) []Bit {
+func (_ modify) ToggleBits(bits ...Bit) []Bit {
 	for i := 0; i < len(bits); i++ {
 		bits[i] ^= One
 	}
 	return bits
 }
 
-// ToggleData XORs every bit of a byte with 1.
-func (_ modify) ToggleData(data []byte) []byte {
+// ToggleBytes XORs every bit of a byte with 1.
+func (_ modify) ToggleBytes(data ...byte) []byte {
 	for i := 0; i < len(data); i++ {
 		data[i] ^= MaxByte
 	}
