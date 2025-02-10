b919353918840156a2bb0d7e94da86f454a48ce2
Robert Petz <rpetz@ignitelabs.net>
2025-02-09 16:44:16 -0800

    Added a way to create random slices of binary information

--- create.go
+++ create.go
@@ -1,5 +1,7 @@
 package tiny
 
+import "crypto/rand"
+
 // create is a way to synthesize binary slices from known parameters.
 type create struct{}
 
@@ -22,6 +24,17 @@ func (_ create) Grey(count int, pattern ...Bit) []Bit {
 	var bits []Bit
 	for i := 0; i < count; i++ {
 		bits = append(bits, pattern...)
+	}
+	return bits
+}
+
+// Random creates a random sequence of 1s and 0s.
+func (_ create) Random(width int) []Bit {
+	bits := make([]Bit, width)
+	for i := 0; i < width; i++ {
+		var b [1]byte
+		_, _ = rand.Read(b[:])
+		bits[i] = Bit(b[0] % 2)
 	}
 	return bits
 }
