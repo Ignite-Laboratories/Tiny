d749c2ef7e75c60304bbe4c9e8810a94d1baadd0
Robert Petz <rpetz@ignitelabs.net>
2025-02-11 18:36:17 -0800

    Added a way to XOR a pattern on a regular interval

--- modify.go
+++ modify.go
@@ -2,11 +2,38 @@ package tiny
 
 type _modify int
 
+// XORWithPatternOnInterval creates a repeating pattern of the provided bits and XORs it against
+// the entire length of the Measure on a regular interval.
+// For example, if you want to XOR 11 with the first two bits of every byte, you would provide a
+// Bit pattern of '11' with an interval of '6'
+func (_ _modify) XORWithPatternOnInterval(measure Measure, interval int, pattern ...Bit) {
+	patternI := 0
+	intervalI := 0
+	skipping := false
+	measure.ForEachBit(func(_ int, bit Bit) Bit {
+		if skipping {
+			intervalI++
+			if intervalI >= interval {
+				skipping = false
+			}
+		} else {
+			bit = bit ^ pattern[patternI]
+			patternI++
+			if patternI >= len(pattern) {
+				skipping = true
+				intervalI = 0
+				patternI = 0
+			}
+		}
+		return bit
+	})
+}
+
 // XORWithPattern creates a repeating pattern of the provided bits and XORs it against
 // the entire length of the Measure.
 func (_ _modify) XORWithPattern(measure Measure, pattern ...Bit) {
 	patternI := 0
-	measure.ForEachBit(func(i int, bit Bit) Bit {
+	measure.ForEachBit(func(_ int, bit Bit) Bit {
 		bit = bit ^ pattern[patternI]
 		patternI++
 		if patternI >= len(pattern) {
