16c19f442040f353a392c16b0362aa49439472ff
Robert Petz <rpetz@ignitelabs.net>
2025-02-17 19:21:06 -0800

    Phrase is confusing now
    Since switching away from "Measure" to "Measurement", "Phrase" is an odd term to use.  It'll still make sense in the future, but for now I want to part ways with it.

--- types.go
+++ types.go
@@ -1,9 +1,5 @@
 package tiny
 
-// Phrase represents a logical unit of measures.  A phrase allows you to break up measures and
-// introduce deviations to the data during processing.
-type Phrase []Measurement
-
 // Shade is a descriptor of whether the binary data is Light, Dark, or Grey.
 type Shade int
 
