186beb34ec5abb077cd5346102c0d48d350006b1
Robert Petz <rpetz@ignitelabs.net>
2025-07-06 16:00:48 -0700

    Added a way to print an index's width out between pipes as a convenience for enigmaneering purposes

--- support.go
+++ support.go
@@ -1,7 +1,10 @@
 package tiny
 
 import (
+	"fmt"
 	"math"
+	"strconv"
+	"strings"
 )
 
 var _bitWidth = -1
@@ -27,5 +30,53 @@ func GetBitWidth(number int) int {
 		return 1
 	} else {
 		return int(math.Floor(math.Log2(float64(number)))) + 1
+	}
+}
+
+// PrintIndexWidth is a convenience method to pretty print the width of an index between pipes.
+// The output will always ensure the distance between the two pipes is the provided width of characters,
+// with the width printed in the center and arrows pointing outwards as such:
+//
+//	|1|
+//	|←→|
+//	|←3→|
+//	|← 4→|
+//	|← 5 →|
+//
+// ...
+//
+//	|←      16      →|
+//
+// NOTE: If you'd like to simply print the pipes and arrows without the width, provide false to the optional showWidth.
+func PrintIndexWidth(width int, showWidth ...bool) string {
+	show := true
+	if len(showWidth) > 0 {
+		show = showWidth[0]
+	}
+	if width < 0 {
+		width = 0
+	}
+
+	digits := len(strconv.Itoa(width))
+	widthStr := fmt.Sprintf("%*v", digits, width)
+	if !show {
+		widthStr = strings.Repeat(" ", digits)
+	}
+
+	switch {
+	case width == 0:
+		return "||"
+	case width == 1:
+		return fmt.Sprintf("|%s|", widthStr)
+	case width == 2:
+		return "|←→|"
+	case width == 3:
+		return fmt.Sprintf("|←%s→|", widthStr)
+	default:
+		totalPadding := width - digits - 2
+		left := totalPadding / 2
+		right := totalPadding - left
+
+		return fmt.Sprintf("|←%*v%s%*v→|", left, "", widthStr, right, "")
 	}
 }
---
+++ testing/support_test.go
@@ -0,0 +1,63 @@
+package testing
+
+import (
+	"github.com/ignite-laboratories/tiny"
+	"regexp"
+	"testing"
+	"unicode/utf8"
+)
+
+func Test_Support_PrintIndexWidth(t *testing.T) {
+	// Separate regex for each output
+	validateWithDigits := regexp.MustCompile(`^\|←\s*\d+\s*→\|$`)
+	validateWithoutDigits := regexp.MustCompile(`^\|←\s*→\|$`)
+
+	for i := 0; i < 129; i++ {
+		// Test with width (with digits)
+		strWith := tiny.PrintIndexWidth(i)
+		strWithout := tiny.PrintIndexWidth(i, false)
+
+		switch {
+		case i == 0:
+			if strWith != "||" && strWithout != "||" {
+				t.Fatalf("expected '||', got %s", strWith)
+			}
+			continue
+		case i == 1:
+			if strWith != "|1|" && strWithout != "|1|" {
+				t.Fatalf("expected '|1|', got %s", strWith)
+			}
+			continue
+		case i == 2:
+			if strWith != "|←→|" && strWithout != "|←→|" {
+				t.Fatalf("expected '|←→|', got %s", strWith)
+			}
+			continue
+		}
+
+		length := utf8.RuneCountInString(strWith)
+		if length != i+2 {
+			t.Fatalf("strWith: expected %d characters between pipes, got %d", i+2, length)
+		}
+		length = utf8.RuneCountInString(strWithout)
+		if length != i+2 {
+			t.Fatalf("strWithout: expected %d characters between pipes, got %d", i+2, length)
+		}
+
+		if !validateWithDigits.MatchString(strWith) {
+			t.Fatalf("strWith does not abstractly match the desired output '|← 𝑛 →|': %s", strWith)
+		}
+		if !validateWithoutDigits.MatchString(strWithout) {
+			t.Fatalf("strWithout does not abstractly match the desired output '|←   →|': %s", strWith)
+		}
+	}
+}
+
+func Test_Support_PrintIndexWidth_NegativeInput(t *testing.T) {
+	strWith := tiny.PrintIndexWidth(-1)
+	strWithout := tiny.PrintIndexWidth(-1, false)
+
+	if strWith != "||" && strWithout != "||" {
+		t.Fatalf("expected '||', got %s", strWith)
+	}
+}
